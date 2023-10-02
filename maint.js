d81d368ca93d371617820b53d141a08f4336e62d
HazarD149 <minhvip01350@gmail.com>
2023-10-02 11:00:25 +0700

    update comment emoji list & toggle muted video

--- maint.js
+++ maint.js
@@ -1,5 +1,6 @@
 import strData from "./db_json/story.json" assert { type: "json" };
 import postData from "./db_json/post.json" assert { type: "json" };
+import postEmoji from "./db_json/emoji.json" assert { type: "json" };
 const App = {
   isLogin: false,
   renderUI: function () {
@@ -42,21 +43,7 @@ const App = {
         </video>
        
         <div class="speaker-wraper">
-          <svg
-            aria-label="Audio is playing"
-            class="x1lliihq x1n2onr6"
-            color="rgb(255, 255, 255)"
-            fill="rgb(255, 255, 255)"
-            height="12"
-            role="img"
-            viewBox="0 0 24 24"
-            width="12"
-          >
-            <title>Audio is playing</title>
-            <path
-              d="M16.636 7.028a1.5 1.5 0 1 0-2.395 1.807 5.365 5.365 0 0 1 1.103 3.17 5.378 5.378 0 0 1-1.105 3.176 1.5 1.5 0 1 0 2.395 1.806 8.396 8.396 0 0 0 1.71-4.981 8.39 8.39 0 0 0-1.708-4.978Zm3.73-2.332A1.5 1.5 0 1 0 18.04 6.59 8.823 8.823 0 0 1 20 12.007a8.798 8.798 0 0 1-1.96 5.415 1.5 1.5 0 0 0 2.326 1.894 11.672 11.672 0 0 0 2.635-7.31 11.682 11.682 0 0 0-2.635-7.31Zm-8.963-3.613a1.001 1.001 0 0 0-1.082.187L5.265 6H2a1 1 0 0 0-1 1v10.003a1 1 0 0 0 1 1h3.265l5.01 4.682.02.021a1 1 0 0 0 1.704-.814L12.005 2a1 1 0 0 0-.602-.917Z"
-            ></path>
-          </svg>
+        <svg aria-label="Audo is muted." class="x1lliihq x1n2onr6" color="rgb(255, 255, 255)" fill="rgb(255, 255, 255)" height="12" role="img" viewBox="0 0 48 48" width="12"><title>Audo is muted.</title><path clip-rule="evenodd" d="M1.5 13.3c-.8 0-1.5.7-1.5 1.5v18.4c0 .8.7 1.5 1.5 1.5h8.7l12.9 12.9c.9.9 2.5.3 2.5-1v-9.8c0-.4-.2-.8-.4-1.1l-22-22c-.3-.3-.7-.4-1.1-.4h-.6zm46.8 31.4-5.5-5.5C44.9 36.6 48 31.4 48 24c0-11.4-7.2-17.4-7.2-17.4-.6-.6-1.6-.6-2.2 0L37.2 8c-.6.6-.6 1.6 0 2.2 0 0 5.7 5 5.7 13.8 0 5.4-2.1 9.3-3.8 11.6L35.5 32c1.1-1.7 2.3-4.4 2.3-8 0-6.8-4.1-10.3-4.1-10.3-.6-.6-1.6-.6-2.2 0l-1.4 1.4c-.6.6-.6 1.6 0 2.2 0 0 2.6 2 2.6 6.7 0 1.8-.4 3.2-.9 4.3L25.5 22V1.4c0-1.3-1.6-1.9-2.5-1L13.5 10 3.3-.3c-.6-.6-1.5-.6-2.1 0L-.2 1.1c-.6.6-.6 1.5 0 2.1L4 7.6l26.8 26.8 13.9 13.9c.6.6 1.5.6 2.1 0l1.4-1.4c.7-.6.7-1.6.1-2.2z" fill-rule="evenodd"></path></svg>
         </div>
         </li>
           `;
@@ -66,12 +53,13 @@ const App = {
         </li>`;
         }
       });
-
       if (item.postImage.length > 1) {
-        indexList = item.postImage.map((item) => {
-          return `
-            <span></span>
-            `;
+        indexList = item.postImage.map((img, index) => {
+          if (index === 0) {
+            return `<span class="activeImg"></span>`;
+          } else {
+            return `<span></span>`;
+          }
         });
       } else {
         indexList = [];
@@ -93,7 +81,7 @@ const App = {
         <div class="post-time">${item.postTime}</div>
       </div>
       <div class="post-slideshow">
-        ${item.postImage.length > 1 ? btnPost : null}
+        ${item.postImage.length > 1 ? btnPost : ""}
         <div class="btn-indexPost">
         ${indexList.join("")}
         </div>
@@ -182,7 +170,13 @@ const App = {
           </div>
           <div class="add-comment">
             <input type="text" name="" id="" placeholder="Add a comment..." />
+            <div class="emoji">
             <i class="ri-emotion-line"></i>
+            <div class="emoji-picker">
+              <ul class="emo-wraper">
+              </ul>
+            </div>
+            </div>
           </div>
         </div>
       </div>
@@ -190,8 +184,142 @@ const App = {
       `;
     });
     $(".posts-container").html(postLists.join(""));
-    // $(".slider-wrap").html(postImgList.join(""));
-    // $(".btn-indexPost").html(indexList.join(""));
+    //render emoji
+    const emoList = postEmoji.map((emoji) => {
+      return `<li class="emo" style="font-size:32px">${emoji.emoji}</li>`;
+    });
+    $(".emo-wraper").append(emoList.join(""));
+  },
+  RenderCommentModal: function (postItem) {
+    //render comment
+    let postImgList;
+    let indexList;
+    const btnPost = `<div class="btn-nextPost btn-post"></div>
+    <div class="btn-prevPost btn-post"></div>`;
+    const post = () => {
+      postImgList = postItem.postImage.map((img) => {
+        if (postItem.postImage.length > 1) {
+          indexList = postItem.postImage.map((img, index) => {
+            if (index === 0) {
+              return `<span class="activeImg"></span>`;
+            } else {
+              return `<span></span>`;
+            }
+          });
+        } else {
+          indexList = [];
+        }
+        if (img.includes(".mp4")) {
+          return `
+          <li class="md_post-content">
+                <video  style="max-height: 700px;" loop="true" autoplay="autoplay" playsinline="" muted="" width="75%">
+                <source src="${img}" type="">
+              </video>
+             
+              <div class="speaker-wraper">
+                <svg aria-label="Audio is playing" class="x1lliihq x1n2onr6" color="rgb(255, 255, 255)" fill="rgb(255, 255, 255)" height="12" role="img" viewBox="0 0 24 24" width="12">
+                  <title>Audio is playing</title>
+                  <path d="M16.636 7.028a1.5 1.5 0 1 0-2.395 1.807 5.365 5.365 0 0 1 1.103 3.17 5.378 5.378 0 0 1-1.105 3.176 1.5 1.5 0 1 0 2.395 1.806 8.396 8.396 0 0 0 1.71-4.981 8.39 8.39 0 0 0-1.708-4.978Zm3.73-2.332A1.5 1.5 0 1 0 18.04 6.59 8.823 8.823 0 0 1 20 12.007a8.798 8.798 0 0 1-1.96 5.415 1.5 1.5 0 0 0 2.326 1.894 11.672 11.672 0 0 0 2.635-7.31 11.682 11.682 0 0 0-2.635-7.31Zm-8.963-3.613a1.001 1.001 0 0 0-1.082.187L5.265 6H2a1 1 0 0 0-1 1v10.003a1 1 0 0 0 1 1h3.265l5.01 4.682.02.021a1 1 0 0 0 1.704-.814L12.005 2a1 1 0 0 0-.602-.917Z"></path>
+                </svg>
+              </div>
+              </li>
+        `;
+        } else {
+          return `
+          <li class="md_post-content">
+                <img style="max-height:700px" src="${img}" alt="">
+              </li>
+          `;
+        }
+      });
+      return `
+      <div class="comment-modal">
+          <div class="postSlide-modal">
+          ${postItem.postImage.length > 1 ? btnPost : ""}
+          <div class="btn-indexPost">
+        ${indexList.join("")}
+        </div>
+            <ul class="md_maint-post">
+              ${postImgList.join("")}
+            </ul>
+          </div>
+          <div class="md_interact-wraper">
+            <div class="md_interact-header">
+              <div class="md_interact-hd-wraper">
+                <div class="post-avt">
+                  <img
+                    height="32"
+                    width="32"
+                    src="./src/images/avatar/278822746_1648530392213148_8157179013739528613_n.jpg"
+                    alt=""
+                  />
+                </div>
+                <div class="post-username">minh_nguyen149</div>
+              </div>
+              <div class="md_post-status">
+                hiphopNVD
+              </div>
+            
+            </div>
+            <div class="md_interact-comment">
+              <div class="md_comment">
+                <div class="md_comment-wraper">
+                  <div class="i4-comment">
+                    <div class="comment-avt">
+                      <img height="32" width="32" src="./src/images/avatar/278822746_1648530392213148_8157179013739528613_n.jpg" alt="">
+                    </div>
+                    <div class="i4-name">minh_nhat1489</div>
+                  </div>
+                  <div class="md-maint-comment">
+                    <p>ngon vl</p>
+                  </div>
+                </div>
+                <div class="md-comment-likes">
+                  <svg aria-label="Like" class="x1lliihq x1n2onr6" color="rgb(245, 245, 245)" fill="rgb(245, 245, 245)" height="12" role="img" viewBox="0 0 24 24" width="12"><title>Like</title><path d="M16.792 3.904A4.989 4.989 0 0 1 21.5 9.122c0 3.072-2.652 4.959-5.197 7.222-2.512 2.243-3.865 3.469-4.303 3.752-.477-.309-2.143-1.823-4.303-3.752C5.141 14.072 2.5 12.167 2.5 9.122a4.989 4.989 0 0 1 4.708-5.218 4.21 4.21 0 0 1 3.675 1.941c.84 1.175.98 1.763 1.12 1.763s.278-.588 1.11-1.766a4.17 4.17 0 0 1 3.679-1.938m0-2a6.04 6.04 0 0 0-4.797 2.127 6.052 6.052 0 0 0-4.787-2.127A6.985 6.985 0 0 0 .5 9.122c0 3.61 2.55 5.827 5.015 7.97.283.246.569.494.853.747l1.027.918a44.998 44.998 0 0 0 3.518 3.018 2 2 0 0 0 2.174 0 45.263 45.263 0 0 0 3.626-3.115l.922-.824c.293-.26.59-.519.885-.774 2.334-2.025 4.98-4.32 4.98-7.94a6.985 6.985 0 0 0-6.708-7.218Z"></path></svg>
+                </div>
+              </div>
+            </div>
+            <div class="md_other-interact">
+              <div class="interact-items">
+                <div class="interact-icons">
+                  <div class="like">
+                    <svg aria-label="Like" class="x1lliihq x1n2onr6" color="rgb(245, 245, 245)" fill="rgb(245, 245, 245)" height="24" role="img" viewBox="0 0 24 24" width="24">
+                      <title>Like</title>
+                      <path d="M16.792 3.904A4.989 4.989 0 0 1 21.5 9.122c0 3.072-2.652 4.959-5.197 7.222-2.512 2.243-3.865 3.469-4.303 3.752-.477-.309-2.143-1.823-4.303-3.752C5.141 14.072 2.5 12.167 2.5 9.122a4.989 4.989 0 0 1 4.708-5.218 4.21 4.21 0 0 1 3.675 1.941c.84 1.175.98 1.763 1.12 1.763s.278-.588 1.11-1.766a4.17 4.17 0 0 1 3.679-1.938m0-2a6.04 6.04 0 0 0-4.797 2.127 6.052 6.052 0 0 0-4.787-2.127A6.985 6.985 0 0 0 .5 9.122c0 3.61 2.55 5.827 5.015 7.97.283.246.569.494.853.747l1.027.918a44.998 44.998 0 0 0 3.518 3.018 2 2 0 0 0 2.174 0 45.263 45.263 0 0 0 3.626-3.115l.922-.824c.293-.26.59-.519.885-.774 2.334-2.025 4.98-4.32 4.98-7.94a6.985 6.985 0 0 0-6.708-7.218Z"></path>
+                    </svg>
+                  </div>
+                  <div class="comments">
+                    <svg aria-label="Comment" class="x1lliihq x1n2onr6" color="rgb(245, 245, 245)" fill="rgb(245, 245, 245)" height="24" role="img" viewBox="0 0 24 24" width="24">
+                      <title>Comment</title>
+                      <path d="M20.656 17.008a9.993 9.993 0 1 0-3.59 3.615L22 22Z" fill="none" stroke="currentColor" stroke-linejoin="round" stroke-width="2"></path>
+                    </svg>
+                  </div>
+                </div>
+                <div class="save">
+                  <svg aria-label="Save" class="x1lliihq x1n2onr6" color="rgb(245, 245, 245)" fill="rgb(245, 245, 245)" height="24" role="img" viewBox="0 0 24 24" width="24">
+                    <title>Save</title>
+                    <polygon fill="none" points="20 21 12 13.44 4 21 4 3 20 3 20 21" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"></polygon>
+                  </svg>
+                </div>
+              </div>
+              <div class="md-total-like">
+                <span>1000</span>
+                <span>likes</span>
+              </div>
+            </div>
+            <div class="md_add-comment">
+              <div class="add-comment">
+                <i class="ri-emotion-line"></i>
+                <input type="text" name="" id="" placeholder="Add a comment..." />
+                <div class="btn-cf-comment">post</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      `;
+    };
+    $(".comment-overlay").append(post);
+    App.handleScrollPost();
   },
   handleSearch: function () {
     if ($(".item-search").hasClass("blue")) {
@@ -249,21 +377,21 @@ const App = {
     const currentIdx = (x) => {
       imgIndex += x;
     };
-    $(".btn-prevPost").hide();
+    // $(".btn-prevPost").hide();
     const toggleBtn = (maintpost) => {
-      console.log(imgIndex);
       const postlenght = maintpost.children(".slider-wrap").children().length;
-      imgIndex > 0 ? $(".btn-prevPost").show() : $(".btn-prevPost").hide();
+      imgIndex > 0
+        ? maintpost.children(".btn-prevPost").show()
+        : maintpost.children(".btn-prevPost").hide();
       imgIndex === postlenght - 1
-        ? $(".btn-nextPost").hide()
-        : $(".btn-nextPost").show();
+        ? maintpost.children(".btn-nextPost").hide()
+        : maintpost.children(".btn-nextPost").show();
     };
-    const width = $(".slider-wrap").width();
+
     const scroll = (width, maintpost) => {
-      $(".slider-wrap").animate({ scrollLeft: "+=" + width + "px" }, 500);
-      setTimeout(() => {
-        toggleBtn(maintpost);
-      }, 500);
+      $(maintpost)
+        .children("ul")
+        .animate({ scrollLeft: "+=" + width + "px" }, 500);
     };
     $(".slider-wrap").on("touchstart", (event) => {
       $(".slider-wrap").addClass("toggleSwip");
@@ -301,25 +429,29 @@ const App = {
     });
     $("div.btn-post").on("click", (el) => {
       const maintpost = $(el.currentTarget).parent();
-      $(`.btn-indexPost span:eq(${imgIndex})`).toggleClass("activeImg");
+      const width = maintpost.children("ul").width();
+      const target = maintpost.find(".btn-indexPost").children(".activeImg");
+      if ($(el.currentTarget).is(".btn-nextPost")) {
+        if (target.next().is(":empty")) {
+          target.toggleClass("activeImg");
+        }
+        target.next().toggleClass("activeImg");
+      } else {
+        if (target.prev().is(":empty")) {
+          target.toggleClass("activeImg");
+        }
+        target.prev().toggleClass("activeImg");
+      }
       $(el.currentTarget).is(".btn-nextPost")
-        ? $(`.btn-indexPost span:eq(${imgIndex})`)
-            .next()
-            .toggleClass("activeImg")
-        : $(`.btn-indexPost span:eq(${imgIndex})`)
-            .prev()
-            .toggleClass("activeImg");
-      $(el.currentTarget).is(".btn-nextPost")
-        ? scroll(width, maintpost) & currentIdx(1)
-        : scroll(-width, maintpost) & currentIdx(-1);
-
-      // $(`.btn-indexPost span:eq(${imgIndex})`).toggleClass("activeImg");
+        ? scroll(width, maintpost)
+        : scroll(-width, maintpost);
     });
   },
   handleShowComment: function () {
     $("div.show-comments").each(function (index) {
       $(this).on("click", () => {
-        console.log(this);
+        App.RenderCommentModal(postData[index]);
+        $(".comment-overlay").show();
       });
     });
   },
@@ -341,6 +473,43 @@ const App = {
     $(".btn-close_comment").on("click", () => {
       $(".comment-overlay").hide();
     });
+
+    $(".emoji").on("click", (el) => {
+      $(el.currentTarget).children(".emoji-picker").toggle();
+    });
+    $(".emoji-picker")
+      .children()
+      .on("click", (e) => {
+        e.stopPropagation();
+      });
+    $(".speaker-wraper").each((idx, spk) => {
+      $(spk).on("click", (el) => {
+        const r = $(el.currentTarget).parent().children("video");
+        if (r.prop("muted", true) && $(this).attr("data-click-state") == 1) {
+          $(this).attr("data-click-state", 0);
+          $(el.currentTarget).html(
+            '<svg aria-label="Audio is playing" class="x1lliihq x1n2onr6" color="rgb(255, 255, 255)" fill="rgb(255, 255, 255)" height="12" role="img" viewBox="0 0 24 24" width="12"><title>Audio is playing</title><path d="M16.636 7.028a1.5 1.5 0 1 0-2.395 1.807 5.365 5.365 0 0 1 1.103 3.17 5.378 5.378 0 0 1-1.105 3.176 1.5 1.5 0 1 0 2.395 1.806 8.396 8.396 0 0 0 1.71-4.981 8.39 8.39 0 0 0-1.708-4.978Zm3.73-2.332A1.5 1.5 0 1 0 18.04 6.59 8.823 8.823 0 0 1 20 12.007a8.798 8.798 0 0 1-1.96 5.415 1.5 1.5 0 0 0 2.326 1.894 11.672 11.672 0 0 0 2.635-7.31 11.682 11.682 0 0 0-2.635-7.31Zm-8.963-3.613a1.001 1.001 0 0 0-1.082.187L5.265 6H2a1 1 0 0 0-1 1v10.003a1 1 0 0 0 1 1h3.265l5.01 4.682.02.021a1 1 0 0 0 1.704-.814L12.005 2a1 1 0 0 0-.602-.917Z"></path></svg>'
+          );
+          r.prop("muted", false);
+        } else {
+          $(this).attr("data-click-state", 1);
+          r.prop("muted", true);
+          $(el.currentTarget).html(
+            '<svg aria-label="Audo is muted." class="x1lliihq x1n2onr6" color="rgb(255, 255, 255)" fill="rgb(255, 255, 255)" height="12" role="img" viewBox="0 0 48 48" width="12"><title>Audo is muted.</title><path clip-rule="evenodd" d="M1.5 13.3c-.8 0-1.5.7-1.5 1.5v18.4c0 .8.7 1.5 1.5 1.5h8.7l12.9 12.9c.9.9 2.5.3 2.5-1v-9.8c0-.4-.2-.8-.4-1.1l-22-22c-.3-.3-.7-.4-1.1-.4h-.6zm46.8 31.4-5.5-5.5C44.9 36.6 48 31.4 48 24c0-11.4-7.2-17.4-7.2-17.4-.6-.6-1.6-.6-2.2 0L37.2 8c-.6.6-.6 1.6 0 2.2 0 0 5.7 5 5.7 13.8 0 5.4-2.1 9.3-3.8 11.6L35.5 32c1.1-1.7 2.3-4.4 2.3-8 0-6.8-4.1-10.3-4.1-10.3-.6-.6-1.6-.6-2.2 0l-1.4 1.4c-.6.6-.6 1.6 0 2.2 0 0 2.6 2 2.6 6.7 0 1.8-.4 3.2-.9 4.3L25.5 22V1.4c0-1.3-1.6-1.9-2.5-1L13.5 10 3.3-.3c-.6-.6-1.5-.6-2.1 0L-.2 1.1c-.6.6-.6 1.5 0 2.1L4 7.6l26.8 26.8 13.9 13.9c.6.6 1.5.6 2.1 0l1.4-1.4c.7-.6.7-1.6.1-2.2z" fill-rule="evenodd"></path></svg>'
+          );
+        }
+      });
+    });
+  },
+  handleEmoPiker: function () {
+    $(".emo").each((el, item) => {
+      $(item).on("click", (e) => {
+        const r = $(e.currentTarget).parents(".add-comment");
+        $(r)
+          .children("input")
+          .val($(r).children("input").val() + e.currentTarget.innerText);
+      });
+    });
   },
   Start: function () {
     this.renderUI();
@@ -348,6 +517,7 @@ const App = {
     this.handleScrollStr();
     this.handleScrollPost();
     this.handleShowComment();
+    this.handleEmoPiker();
   },
 };
 App.Start();
