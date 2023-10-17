16877ba2bbb3c75c37b8a80a40f88af0a36bc823
HazarD149 <minhvip01350@gmail.com>
2023-10-18 01:03:45 +0700

    setup api customer service

---
+++ be/customer/src/api/customer.js
@@ -0,0 +1,8 @@
+module.exports = (app) => {
+  app.post("/register", async (req, res, next) => {});
+  app.post("/login", async (req, res, next) => {});
+  app.post("/updateProfile", async (req, res, next) => {});
+  app.get("/profile", async (req, res, next) => {});
+  app.post("/follow", async (req, res, next) => {});
+  app.delete("/delUser", async (req, res, next) => {});
+};
--- be/customer/src/index.js
+++ be/customer/src/index.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const app = express();
 const port = 3001;
-
+const customer = require("./api");
 require("./bd-connection");
 require("./associate");
 app.use(express.json());
-
+app.use(cors());
+customer(app);
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
